dff48c323f844e6c41ca1b2fdf303228135f999c
Dimyati Utoyo <dimyatiutoyo.b@gmail.com>
2022-05-27 15:50:39 +0700

    add variable to save favoriteColor and favoriteRestaurant temporary

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ firstUser.name = 'Monica';
 firstUser.gender = 'Female';
 firstUser.age = 17;
 firstUser.email = 'monica@dingdong.com';
-firstUser.favoriteColor = new Set();
-firstUser.favoriteColor.add('Yellow');
-firstUser.favoriteColor.add('Pink');
-firstUser.favoriteColor.add('White');
-firstUser.favoriteColor.add('Purple');
+
+let favoriteColor1 = new Set()
+favoriteColor1.add('Yellow');
+favoriteColor1.add('Pink');
+favoriteColor1.add('White');
+favoriteColor1.add('Purple');
+firstUser.favoriteColor = [...favoriteColor1];
+
 firstUser.isHavePet = true;
 firstUser.education = new Array();
 firstUser.education.push({
@@ -28,25 +31,33 @@ firstUser.education.push({
     city: 'Tangerang',
     graduate: ''
 });
-firstUser.favoriteRestaurant = new Set();
-firstUser.favoriteRestaurant.add('Bento');
-firstUser.favoriteRestaurant.add('Sushi');
-firstUser.favoriteRestaurant.add('Pancake');
-firstUser.favoriteRestaurant.add('Eggy');
-firstUser.favoriteRestaurant.add('Tempura');
-firstUser.favoriteRestaurant.add('Padang');
-firstUser.favoriteRestaurant.add('Tteok');
 
+let favoriteRestaurant1 = new Set();
+favoriteRestaurant1.add('Bento');
+favoriteRestaurant1.add('Sushi');
+favoriteRestaurant1.add('Pancake');
+favoriteRestaurant1.add('Eggy');
+favoriteRestaurant1.add('Tempura');
+favoriteRestaurant1.add('Bento');
+favoriteRestaurant1.add('Eggy');
+favoriteRestaurant1.add('Padang');
+favoriteRestaurant1.add('Tteok');
+favoriteRestaurant1.add('Sushi');
+favoriteRestaurant1.add('Sushi');
+firstUser.favoriteRestaurant = [...favoriteRestaurant1];
 
 const secondUser = new Object();
 secondUser.name = 'Wendy';
 secondUser.gender = 'Male';
 secondUser.age = 23;
 secondUser.email = 'wendy@dingdong.com';
-secondUser.favoriteColor = new Set();
-secondUser.favoriteColor.add('Blue');
-secondUser.favoriteColor.add('Black');
-secondUser.favoriteColor.add('Grey');
+
+let favoriteColor2 = new Set()
+favoriteColor2.add('Blue');
+favoriteColor2.add('Black');
+favoriteColor2.add('Grey');
+secondUser.favoriteColor = [...favoriteColor2];
+
 secondUser.isHavePet = false;
 secondUser.education = new Array();
 secondUser.education.push({
@@ -69,15 +80,19 @@ secondUser.education.push({
     city: 'Tangerang',
     graduate: ''
 });
-secondUser.favoriteRestaurant = new Set();
-secondUser.favoriteRestaurant.add('Tempura');
-secondUser.favoriteRestaurant.add('Bento');
-secondUser.favoriteRestaurant.add('Sushi');
-secondUser.favoriteRestaurant.add('Pancake');
-secondUser.favoriteRestaurant.add('Padang');
-secondUser.favoriteRestaurant.add('Katsu');
-secondUser.favoriteRestaurant.add('Geprek');
-secondUser.favoriteRestaurant.add('Eggy');
+
+let favoriteRestaurant2 = new Set();
+favoriteRestaurant2.add('Tempura');
+favoriteRestaurant2.add('Bento');
+favoriteRestaurant2.add('Sushi');
+favoriteRestaurant2.add('Pancake');
+favoriteRestaurant2.add('Padang');
+favoriteRestaurant2.add('Katsu');
+favoriteRestaurant2.add('Geprek');
+favoriteRestaurant2.add('Pancake');
+favoriteRestaurant2.add('Eggy');
+
+secondUser.favoriteRestaurant = [...favoriteRestaurant2];
 
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
